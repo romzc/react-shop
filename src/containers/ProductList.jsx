9175323b449215687c2016ad0b22c865b916a389
Rony <ronyramos495@gmail.com>
2022-04-26 10:42:07 -0500

    axios installed and get data

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import ProductItem from '../components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios'
+
+const API = 'https://api.escuelajs.co/api/v1/products'
 
 const ProductList = () => {
+	const [products, setProducts] = React.useState([])
+
+	React.useEffect(()=>{
+		async function fetchData() {
+			const response = await axios.get(API)
+			setProducts(response.data)
+		}
+		fetchData()
+	},[])
+
+	const productList = products.map((prod, index) => (<ProductItem key={index} />))
+
+	console.log(products)
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{productList}
 			</div>
 		</section>
 	);
